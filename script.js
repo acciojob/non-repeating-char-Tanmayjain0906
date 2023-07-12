18a9e2c6f2361815ad235891cc6de4c7a9efb132
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-12 13:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let sentence = prompt("Enter a String");
+let obj = {};
+for(let i=0;i<sentence.length;i++)
+{
+    if(obj[sentence[i]]>=1)
+    {
+        obj[sentence[i]]++;
+    }
+    else
+    {
+        obj[sentence[i]] = 1;
+    }
+}
+
+for(let i=0;i<sentence.length;i++)
+{
+    if(obj[sentence[i]]==1)
+    {
+        console.log(sentence[i]);
+        break;
+    }
+}
